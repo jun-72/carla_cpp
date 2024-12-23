a5b267efb5594ea72be94ed4afdebf3c16d80f4c
jun-72 <726945401@qq.com>
2024-12-23 18:34:29 +0800

    添加注释

--- Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Walker/WalkerBoneControlIn.h
+++ Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Walker/WalkerBoneControlIn.h
@@ -8,12 +8,20 @@
 
 #include "WalkerBoneControlIn.generated.h"
 
+// 声明一个结构体 FWalkerBoneControlIn，表示“Walker 骨骼控制输入”的数据结构
+// 使用 Unreal Engine 的宏 USTRUCT，允许它被蓝图系统访问
 USTRUCT(BlueprintType)
 struct CARLA_API FWalkerBoneControlIn
 {
   GENERATED_BODY()
 
+  // 定义一个 UPROPERTY，表示一个可以在编辑器中查看和修改的属性
+  // Category: 指定该属性所在的类别，用于编辑器中的组织
+  // EditAnywhere: 表示该属性可以在任何地方编辑
+  // BlueprintReadWrite: 表示该属性可以在蓝图中读写
   UPROPERTY(Category = "Walker Bone Control", EditAnywhere, BlueprintReadWrite)
+  // 该属性表示一个键值对映射（TMap），键为 FString 类型，值为 FTransform 类型
+  // 这个映射用于存储与骨骼相关的变换信息
   TMap<FString, FTransform> BoneTransforms;
 
 };
